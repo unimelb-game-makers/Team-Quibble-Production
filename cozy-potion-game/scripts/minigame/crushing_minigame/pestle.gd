e61570f1f87bfc15a2eba4d9c75c805765c681c5
Hamish Luke <hamishrluke@gmail.com>
2026-08-21 19:48:20 +1000

    updated: recipe minigame

--- cozy-potion-game/scripts/minigame/crushing_minigame/pestle.gd
+++ cozy-potion-game/scripts/minigame/crushing_minigame/pestle.gd
@@ -1,6 +1,8 @@
 class_name Pestle extends CharacterBody2D
 ## a character body that moves towards the mouse with some velocity
 
+
+
 @export var max_speed: float
 @export var curve: Curve
 
@@ -8,7 +10,7 @@ func _physics_process(_delta: float) -> void:
 	#direction to move
 	var mouse_world_position = get_viewport().get_mouse_position()
 	var move_dir = (mouse_world_position - position).normalized()
-	
+	 
 	#the speed we move towards the cursor is dependent on how far away we are from it
 	# in relation to how far away we could be from it
 	var max_distance = sqrt(get_window().size.x*get_window().size.x + get_window().size.y * get_window().size.y)
