44e73280c5b080b15d8c8895b7a4f40de3e8adf3
Ramsey Morris <118049528+razorarcanum@users.noreply.github.com>
2026-08-22 18:00:28 +1000

    customer can accept or refuse a potion, then spawn another

--- cozy-potion-game/scripts/world/customer_manager.gd
+++ cozy-potion-game/scripts/world/customer_manager.gd
@@ -7,18 +7,37 @@ extends Node
 
 func _ready() -> void:
 	send_customer()
+	customer_interactable.interacted.connect(_on_customer_interact)
 
 func send_customer() -> void:
 	customer_anim_player.play("person_in")
-	customer_interactable.interacted.connect(_on_customer_interact, ConnectFlags.CONNECT_ONE_SHOT)
 	customer.requested_potion_type = Potion.ATTRIBUTES.values().pick_random()
 	customer.has_conveyed_request = false
 
-func _on_customer_interact():
+func recall_customer() -> void:
+	customer_anim_player.play_backwards("person_in")
+	await get_tree().create_timer(1).timeout
+	send_customer()
+
+func _on_customer_interact() -> void:
 	if not customer.has_conveyed_request:
 		#this is, generally, poor use of enums
 		var dialogue_start: String = Potion.ATTRIBUTES.find_key(customer.requested_potion_type)
 		dialogue_start = dialogue_start.to_lower()
 		DialogueManager.show_example_dialogue_balloon(dialogue_resource, dialogue_start)
 		customer.has_conveyed_request = true
-		
+	else:
+		var player: WorldPlayer = get_tree().get_first_node_in_group("Player")
+		if check_potion_match(player.potion, customer.requested_potion_type):
+			DialogueManager.show_example_dialogue_balloon(dialogue_resource, "accept")
+			await DialogueManager.dialogue_ended
+			player.potion = null
+			recall_customer()
+		else:
+			DialogueManager.show_example_dialogue_balloon(dialogue_resource, "refuse")
+			player.potion = null
+
+func check_potion_match(player_potion: Potion, customer_request_type: Potion.ATTRIBUTES) -> bool:
+	if player_potion == null:
+		return false
+	return player_potion.type == customer_request_type
