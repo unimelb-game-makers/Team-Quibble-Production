0f6d9208af5ea5afbb3dba3ca17f437cc697c804
Ramsey Morris <118049528+razorarcanum@users.noreply.github.com>
2026-08-27 17:14:11 +1000

    created new json getter helper in utils
    applied across program hopefully with no changes to functionality

--- cozy-potion-game/scripts/autoloads/utils.gd
+++ cozy-potion-game/scripts/autoloads/utils.gd
@@ -7,3 +7,13 @@ const Group = {
 	GROUP_INTERACTABLE_OBJECTS = "interactable_objects",
 	GROUP_POPUP_SUBWINDOW = "popup_subwindow"
 }
+
+static func get_json(path: String) -> JSON:
+	var json_string: String = FileAccess.open(path, FileAccess.READ).get_as_text()
+	var json_data: JSON = JSON.new()
+
+	var parse_result = json_data.parse(json_string)
+	if parse_result != OK:
+		print_debug("Variable json_data was null. %s" % [json_data.get_error_message()])
+		return null
+	return json_data
--- cozy-potion-game/scripts/customer/customer.gd
+++ cozy-potion-game/scripts/customer/customer.gd
@@ -5,7 +5,7 @@ var problem_severities: Dictionary[String, float]
 var needs: Array[String]
 
 const TYPE_TO_NEEDS_JSON_PATH: String = "res://scripts/customer/customer_type_to_need_probability.json"
-
+const GAME_STAGE_TO_AILMENT_CHANCE_JSON_PATH: String = "res://scripts/customer/game_stage_to_ailment_chance.json"
 ##takes a potion as an argument and returns true if the potion will be accepted
 ## by this customer, otherwise false. A potion will be accepted if.. well, i'm
 ## not sure yet. TODO: update this function after the rest of the potion system
@@ -25,4 +25,9 @@ static func generate_customer() -> Customer:
 	var customer_types = json_data.data["NPC"]
 	var customer_index = randi_range(0, customer_types.size()-1)
 	customer.customer_type = customer_types[customer_index]
+	
+	var ailment_chance_array = 
+	
 	return customer
+
+	
