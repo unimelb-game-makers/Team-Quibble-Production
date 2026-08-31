41c1eeaac4c1db022068ce9829cbb40f8df4b63e
Ramsey Morris <118049528+razorarcanum@users.noreply.github.com>
2026-08-31 14:43:23 +1000

    added news manager autoload

---
+++ cozy-potion-game/scripts/autoloads/news_manager.gd
@@ -0,0 +1,8 @@
+extends Node
+
+var current_news_event: Dictionary
+
+const NEWS_JSON_PATH: String = "res://scripts/autoloads/news_events.json"
+
+func generate_news_event() -> Dictionary:
+	return {"placeholder":"placeholder"}
