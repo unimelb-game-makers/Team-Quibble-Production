13ec8f09e6a3205bd6784f7a4ab68f41f838ead3
Nemo115 <43259251+Nemo115@users.noreply.github.com>
2026-09-09 12:06:19 +1000

    floating ui

--- cozy-potion-game/scripts/ui/indicator.gd
+++ cozy-potion-game/scripts/ui/indicator.gd
@@ -3,6 +3,12 @@ class_name Indicator
 
 var pivot_pos: Vector3
 
+func _ready() -> void:
+	var tw: Tween = get_tree().create_tween()
+	tw.set_loops(-1)
+	tw.tween_property($PanelContainer, "offset_transform_position:y", -30, 1).set_trans(Tween.TRANS_QUAD)
+	tw.tween_property($PanelContainer, "offset_transform_position:y", 0, 1).set_trans(Tween.TRANS_QUAD)
+
 func set_pivot(pos: Vector3):
 	pivot_pos = pos
 
