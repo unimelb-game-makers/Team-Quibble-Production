ebb2511807f0adb33e2ca2aaf02a5dc8fb46af87
Ramsey Morris <118049528+razorarcanum@users.noreply.github.com>
2026-09-12 11:07:55 +1000

    bugfixing

--- cozy-potion-game/scripts/popup_sub_window.gd
+++ cozy-potion-game/scripts/popup_sub_window.gd
@@ -50,8 +50,8 @@ func start_display_popup(_scene_to_load: PackedScene, hotbar: Inventory,\
 		popup.ingredient_processed.connect(hotbar.add_new_slot_from_stack)
 
 func end_display_popup(output_hotbar: Inventory) -> void:
-	if output_hotbar != null:
-		player.hotbar.assign_new_inventory(output_hotbar)
+	#if output_hotbar != null:
+		#player.hotbar.assign_new_inventory(output_hotbar)
 	
 	animation_player.play_backwards(&"fade_in")
 	await animation_player.animation_finished
