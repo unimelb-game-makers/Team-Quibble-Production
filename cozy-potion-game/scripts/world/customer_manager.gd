0562eb30006fce1f8124c60b8a91131ad11e8ab3
Ramsey Morris <118049528+razorarcanum@users.noreply.github.com>
2026-09-22 20:38:44 +1000

    merge fixes

--- cozy-potion-game/scripts/world/customer_manager.gd
+++ cozy-potion-game/scripts/world/customer_manager.gd
@@ -143,10 +143,10 @@ func get_next_customer() -> Customer:
 	return customer_queue.pop_front()
 	
 func customer_tests() -> void:
-	var test_customer_1: Customer = Customer.generate_customer(-1, false)
+	var test_customer_1: Customer = Customer.generate_customer(-1)
 	assert(test_customer_1.customer_id >= 0, "bad customer 1")
 	assert(test_customer_1.needs.size() > 0, "bad customer 1")
-	var test_customer_2: Customer = Customer.generate_customer(Customer.CustomerID.NPC_STUDENT, false)
+	var test_customer_2: Customer = Customer.generate_customer(Customer.CustomerID.NPC_STUDENT)
 	assert(test_customer_2.customer_id == Customer.CustomerID.NPC_STUDENT, "bad customer 2")
 	assert(test_customer_2.needs.size() > 0, "bad customer 2")
 
