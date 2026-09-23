0dd1f75264326ce8210c23d8fc80d4b0c164856a
Dylan Wotherspoon <dylan.wotherspoon.au@gmail.com>
2026-09-23 15:15:30 +1000

    clickable version of draggable

--- cozy-potion-game/scenes/user_interface/clickable_drag_component.gd
+++ cozy-potion-game/scenes/user_interface/clickable_drag_component.gd
@@ -1,5 +1,5 @@
 ## add as child of a ui element that you want to be draggable
-class_name DraggableComponent extends Node
+class_name ClickableComponent extends Node
 #this code and the coupled DraggableAcceptor make liberal use of get_parent()
 #which is generally bad when overused but there is no other oppurtunity
 
@@ -16,21 +16,46 @@ var my_control: Control
 
 #use these in the control that uses this component if you want
 #functionality when dragging or dropping
-signal draggable_dropped
-signal draggable_accepted
-signal draggable_picked_up
+signal clickable_dropped
+signal clickable_picked_up
 
 func _ready() -> void:
 	assert(get_parent() is Control, "draggable component not child of control")
 	my_control = get_parent()
+	
+	set_process(false)
 
 
+# Only runs if being dragged
 func _process(_delta: float) -> void:
 	if dragged_control == my_control:
 		move_to_mouse()
+		
+		# Following Inputs will try to place component
+		if Input.is_action_pressed("LMB"):
+			# If no new pending parent to
+			if not pending_parent or pending_parent == previous_parent:
+				return_to_previous()
+			else:
+				# Listen to acceptor
+				pass
+			pass
+		elif Input.is_action_pressed("RMB"):
+			#list to acceptor but for RMB
+			pass
 
 
 func _unhandled_input(event: InputEvent) -> void:
+	if not being_dragged:
+		# Start dragging if cursor hovering & nothing else is being dragged
+		if event.is_action_pressed("LMB") and\
+			my_control.get_global_rect().has_point(\
+			my_control.get_global_mouse_position()) and \
+			dragged_control == null:
+			
+			assign_to_mouse()
+	
+	
 	if event.is_action_released("LMB"):
 		being_dragged = false
 		my_control.top_level = false
@@ -55,18 +80,20 @@ func _unhandled_input(event: InputEvent) -> void:
 
 
 func assign_to_mouse() -> void:
-	draggable_picked_up.emit()
+	clickable_picked_up.emit()
 	dragged_control = my_control
 	previous_parent = my_control.get_parent()
 	my_control.top_level = true
 	being_dragged = true
+	
+	set_process(true)
 
 func move_to_mouse() -> void:
 	my_control.global_position = my_control.get_global_mouse_position() - my_control.get_global_rect().size/2
 
 # Called when dragged to ensure returns to orignal owner
 func return_to_previous() -> void:
-	draggable_dropped.emit()
+	#draggable_dropped.emit()
 	being_dragged = false
 	my_control.reparent(previous_parent)
 	my_control.position = Vector2.ZERO
@@ -80,7 +107,7 @@ func parent_to_acceptor() -> void:
 		get_acceptor(pending_parent).emit_accepted(my_control)
 	my_control.reparent(pending_parent)
 	pending_parent = null
-	draggable_accepted.emit()
+	#draggable_accepted.emit()
 	
 
 func get_acceptor(node: Node) -> DraggableAcceptorComponent:
